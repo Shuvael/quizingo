be99f051297427ecab01cd01609c91d343f4ad9f
Shuvael <65926308+Shuvael@users.noreply.github.com>
2026-06-26 02:14:06 +0200

    - localhost-Platzhalter ersetzt

--- src/app/page.js
+++ src/app/page.js
@@ -34,8 +34,7 @@ export default function Home() {
   );
 
   const raumErstellen = async () => {
-    const res = await fetch("http://localhost:8000/raum/erstellen", {
-      method: "POST",
+const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/raum/erstellen`, {      method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name: spielerName }),
     });
